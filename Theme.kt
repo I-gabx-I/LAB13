12c1b06e1bc384a032a7236702459233d0a086dd
I-gabx-I <157629028+I-gabx-I@users.noreply.github.com>
2025-04-06 23:51:59 -0600

    Create Theme.kt

---
+++ Theme.kt
@@ -0,0 +1,24 @@
+package com.example.lab13.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightColors = lightColorScheme(
+    primary = Color(0xFF6200EE),
+    secondary = Color(0xFF03DAC6),
+    background = Color(0xFFFFFFFF),
+)
+
+@Composable
+fun BMICalculatorTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = LightColors,
+        typography = Typography(),
+        content = content
+    )
+}
